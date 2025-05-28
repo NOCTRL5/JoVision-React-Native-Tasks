88ee9356bab1fa500f104f929e16a201a5ce0a8d
NOCTRL5 <osamatawara7@gmail.com>
2025-05-28 16:37:20 +0300

    task23 react

--- Tasks/Task23.jsx
+++ Tasks/Task23.jsx
@@ -2,11 +2,20 @@ import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { Task23Component } from '../Components/Task23Component';
 
-export default function Task22() {
+export default function Task23() {
+    const [text, setText] = useState("Task23")
+
+    const styles = {
+        textStyle: {
+            fontSize: 18,
+            color: "blue",
+            textAlign: "center",
+        }
+    }
     return (
         <View>
-            <Text>
-                "yes"
+            <Text style={styles.textStyle}>
+                {text}
             </Text>
             <Task23Component setText={setText} />
         </View>
