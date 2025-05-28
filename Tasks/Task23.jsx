d78c176d2cf150e94b127b2b74329426c63877f0
NOCTRL5 <osamatawara7@gmail.com>
2025-05-28 16:43:02 +0300

    moved styles outside to improve performance and to avoid multiple renders

--- Tasks/Task23.jsx
+++ Tasks/Task23.jsx
@@ -3,15 +3,8 @@ import { Text, View } from 'react-native';
 import { Task23Component } from '../Components/Task23Component';
 
 export default function Task23() {
-    const [text, setText] = useState("Task23")
+    const [text, setText] = useState("Task23");
 
-    const styles = {
-        textStyle: {
-            fontSize: 18,
-            color: "blue",
-            textAlign: "center",
-        }
-    }
     return (
         <View>
             <Text style={styles.textStyle}>
@@ -21,3 +14,11 @@ export default function Task23() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 18,
+        color: "blue",
+        textAlign: "center",
+    },
+});
