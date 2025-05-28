76eb13ada83ec3bc1b751cdb737a63db8616b9ae
NOCTRL5 <osamatawara7@gmail.com>
2025-05-28 16:38:22 +0300

    task23component

--- Components/Task23Component.jsx
+++ Components/Task23Component.jsx
@@ -1,1 +1,15 @@
 import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput } from "react-native";
+
+export class Task23Component extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           text_change: function text_change(text) {
+                props.setText(text);
+            }
+        }
+    }
+
+    
+}
