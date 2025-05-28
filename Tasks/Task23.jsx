afd381a5bcb962e2feb1d2f79337e95bb807bf8c
NOCTRL5 <osamatawara7@gmail.com>
2025-05-28 16:51:11 +0300

    redid task23

--- Tasks/Task23.jsx
+++ Tasks/Task23.jsx
@@ -1,18 +1,27 @@
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Task23Component } from '../Components/Task23Component';
 
-export default function Task23() {
-    const [text, setText] = useState("Task23");
+export default class Task23 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "Initial Text"
+        };
+    }
 
-    return (
-        <View>
-            <Text style={styles.textStyle}>
-                {text}
-            </Text>
-            <Task23Component setText={setText} />
-        </View>
-    );
+    updateText = (newText) => {
+        this.setState({ text: newText });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{this.state.text}</Text>
+                <Task23Component setText={this.updateText} />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
