324d58255b6f627f7f13efadfcc811ad6854f64b
NOCTRL5 <osamatawara7@gmail.com>
2025-05-29 16:10:26 +0300

    Task24

---
+++ Tasks/Task24.jsx
@@ -0,0 +1,19 @@
+import { useRef} from "react";
+import { View, TextInput } from "react-native";
+import { Task24FunPage } from "../Components/Task24FunPage";
+import React from "react";
+
+export default function Task24() {
+    const textRef = useRef();
+
+    const on_text_input = (new_text) => {
+        textRef.current.updateText(new_text);
+    }
+    return (
+        <View>
+            <Task24FunPage text={textRef} />
+            <TextInput onChangeText={on_text_input}
+            placeholder="Type here"/>
+        </View>
+    )
+}
