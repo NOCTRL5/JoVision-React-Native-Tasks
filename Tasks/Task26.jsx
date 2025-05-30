384bc3dddee0a9979cd55f8bb04d9c30e1accd09
NOCTRL5 <osamatawara7@gmail.com>
2025-05-30 23:34:46 +0300

    Added Task 26 Function

--- Tasks/Task26.jsx
+++ Tasks/Task26.jsx
@@ -1,3 +1,28 @@
-import React from 'react';
-import { useState } from "react";
-import { ActivityIndicator, Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export function Task26() {
+    const url = "https://api.ipify.org/?format=json";
+
+    const [text, setText] = useState("Task26");
+    const [loader, setLoader] = useState(false);
+
+    const non_blocking_request = () => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setText(`Your IP: ${data.ip}`);
+            })
+            .catch((error) => {
+                setText("Failed to fetch IP");
+                console.error(error);
+            });
+    };
+
+}
