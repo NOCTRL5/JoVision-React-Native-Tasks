ef0d80add8038542ab915fbe96d3f1f857441b32
NOCTRL5 <osamatawara7@gmail.com>
2025-06-01 16:31:05 +0300

    Added images list

--- Tasks/Task28.jsx
+++ Tasks/Task28.jsx
@@ -2,5 +2,18 @@ import { useState } from "react";
 import { Alert, Button, Image, ScrollView, Text, View, FlatList } from "react-native";
 
 export default function Task28() {
-    
+    const images_list = [
+        require('../Resources/0.jpg'),
+        require('../Resources/1.jpg'),
+        require('../Resources/2.jpg'),
+        require('../Resources/3.jpg'),
+        require('../Resources/4.jpg'),
+        require('../Resources/5.jpg'),
+        require('../Resources/6.jpg'),
+        require('../Resources/7.jpg'),
+        require('../Resources/8.jpg'),
+        require('../Resources/9.jpg'),
+        require('../Resources/10.jpg'),
+        require('../Resources/11.gif')
+    ]
 }
