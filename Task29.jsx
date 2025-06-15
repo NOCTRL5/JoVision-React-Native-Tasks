1adf0c46d6a938247b40179fee8ded7884bbb7ad
NOCTRL5 <osamatawara7@gmail.com>
2025-06-15 14:56:02 +0300

    Task 29

--- Task29.jsx
+++
@@ -1,5 +0,0 @@
-import { useRef, useState } from 'react';
-import { Button,FlatList,Modal,View,Platform,StyleSheet } from "react-native";
-import { Popup } from 'react-native-windows';
-import { TaskComp } from '../Components/Task28Comp';
-import { Task29_PopUpPrompt } from '../Components/Task29PopUp';
